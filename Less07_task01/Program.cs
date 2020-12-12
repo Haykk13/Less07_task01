5e3a989d5cef3ef5535127a569d74c97f36313e8
Hayk <hayk.hayr3@gmail.com>
2020-12-13 01:09:24 +0400

    Add project files.

---
+++ Less07_task01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Less07_task01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyStruct inst = new MyStruct(4);
+
+			Console.WriteLine(
+"{0}\t{1}\t{2}\t", inst.X, inst.Y, inst.Z);
+
+			Console.ReadKey();
+		}
+	}
+
+	struct MyStruct
+	{
+		int x;
+		int y;
+		int z;
+
+		public MyStruct(int x, int y, int z)
+			: this(x, y)
+		{
+			this.z = z;
+		}
+
+		public MyStruct(int x, int y) : this(x)
+		{
+			this.y = y;
+		}
+
+		public MyStruct(int x) : this()
+		{
+			this.x = x;
+		}
+
+		public int X { get { return x; } }
+		public int Y { get { return y; } }
+		public int Z { get { return z; } }
+	}
+}
